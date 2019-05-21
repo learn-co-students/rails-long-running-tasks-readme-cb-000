091d5109c9474f39d2e99173a30fc75dd8d9ae32
github username <github email address>
2019-05-20 20:11:09 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   resources :customers, only: [:index]
+  post 'customers/upload', to: 'customers#upload'
 end
+
+# adding the routes to post customer uploads
+# then add the controls to upload a file to our index view page:
